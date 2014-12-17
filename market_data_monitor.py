794658d59f7c1b45cd0c5912225f9a3adc3d38a1
bitjjj <bitjjj@foxmail.com>
2014-12-17 14:56:03 +0800

    update

--- market_data_monitor.py
+++ market_data_monitor.py
@@ -11,7 +11,7 @@ import time
 import math
 import string
 import redis
-import settings
+import settings as config
 
 from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication.row_event import (
@@ -37,43 +37,43 @@ def main():
         statPrefix = "%s:%s:" % (binlogevent.schema,"statistics")
 
         for row in binlogevent.rows:
-	    if isinstance(binlogevent, UpdateRowsEvent) and (row["after_values"]["ctime"] < now):
-			continue#filter old data
-        elif isinstance(binlogevent, UpdateRowsEvent) and (binlogevent.table.find(config.DB_SETTINGS["newdata"]) != -1):
-            vals = row["after_values"]	
+			if isinstance(binlogevent, UpdateRowsEvent) and (row["after_values"]["ctime"] < now):
+				continue#filter old data
+			elif isinstance(binlogevent, UpdateRowsEvent) and (binlogevent.table.find(config.DB_SETTINGS["newdata"]) != -1):
+				vals = row["after_values"]	
+				
+				r.set("lastDbUpdateTime",int(math.floor(time.time())))
+				result = {}
+				for k,v in vals.items():
+					if isinstance(v,basestring):
+						result[k.encode("utf-8")] = v.encode("utf-8")
+					else:
+						result[k.encode("utf-8")] = v 
+				r.set("R_" + vals["symbol"],result)
+						
+				if config.STAT_SETTINGS["enabled"] == "true":	
+					vals["date"] = datetime.now()
+					vals["event"] = time.time()
+							r.hmset(prefix + vals["symbol"], vals)
+					r.incr(statPrefix + "ALL")		
+
+					eventTime = int(math.floor(vals["event"]))
+					cTime = vals["ctime"]
+					diffTime = eventTime - cTime
+					#print eventTime,cTime,diffTime
+					if diffTime <= 1:
+						r.incr(statPrefix + "1s")
+					elif diffTime <= 3:
+						r.incr(statPrefix + "3s")
+					elif diffTime <= 5:
+						r.incr(statPrefix + "5s")
+					elif diffTime <= 10:
+						r.incr(statPrefix + "10s")
+					elif diffTime > 10:
+						r.incr(statPrefix + "B10")
+
 			
-			r.set("lastDbUpdateTime",int(math.floor(time.time())))
-			result = {}
-			for k,v in vals.items():
-				if isinstance(v,basestring):
-					result[k.encode("utf-8")] = v.encode("utf-8")
-				else:
-					result[k.encode("utf-8")] = v 
-			r.set("R_" + vals["symbol"],result)
-					
-			if config.STAT_SETTINGS["enabled"] == "true":	
-				vals["date"] = datetime.now()
-				vals["event"] = time.time()
-						r.hmset(prefix + vals["symbol"], vals)
-				r.incr(statPrefix + "ALL")		
-
-				eventTime = int(math.floor(vals["event"]))
-				cTime = vals["ctime"]
-				diffTime = eventTime - cTime
-				#print eventTime,cTime,diffTime
-				if diffTime <= 1:
-					r.incr(statPrefix + "1s")
-				elif diffTime <= 3:
-					r.incr(statPrefix + "3s")
-				elif diffTime <= 5:
-					r.incr(statPrefix + "5s")
-				elif diffTime <= 10:
-					r.incr(statPrefix + "10s")
-				elif diffTime > 10:
-					r.incr(statPrefix + "B10")
-
-		
-			print datetime.now(),time.time(),vals["ctime"],vals["symbol"],vals["bid"],vals["ask"],vals["high"],vals["low"]
+				print datetime.now(),time.time(),vals["ctime"],vals["symbol"],vals["bid"],vals["ask"],vals["high"],vals["low"]
             '''elif isinstance(binlogevent, UpdateRowsEvent) and (binlogevent.table.find(config.DB_SETTINGS["finance"]) != -1):
                 vals = row["after_values"]
                 r.hmset(prefix + str(vals["id"]), vals)
