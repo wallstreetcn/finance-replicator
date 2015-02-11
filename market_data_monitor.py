a201cda895cea595f8fbbd0ed8482fa154ba4a0d
mr5 <u@mr-5.cn>
2015-02-11 10:53:36 +0800

    insert multiple

--- market_data_monitor.py
+++ market_data_monitor.py
@@ -22,7 +22,7 @@ from pymysqlreplication.row_event import (
 
 
 def main():
-    r = redis.Redis(host=config.REDIS_SETTINGS["host"], port=config.REDIS_SETTINGS["port"])
+    redisConn = redis.Redis(host=config.REDIS_SETTINGS["host"], port=config.REDIS_SETTINGS["port"])
 
     stream = BinLogStreamReader(
         connection_settings=config.MYSQL_SETTINGS,
@@ -37,7 +37,7 @@ def main():
     for binlogevent in stream:
         prefix = "%s:%s:" % (binlogevent.schema, binlogevent.table)
         statPrefix = "%s:%s:" % (binlogevent.schema, "statistics")
-
+        data2redis = {}
         for row in binlogevent.rows:
             # print str(binlogevent.table)
             if binlogevent.table.find(config.DB_SETTINGS["newdata"]) == -1:
@@ -51,7 +51,7 @@ def main():
                         binlogevent.table.find(config.DB_SETTINGS["newdata"]) != -1):
                 vals = row["after_values"]
                 # print vals
-                r.set("lastDbUpdateTime", int(math.floor(time.time())))
+                redisConn.set("lastDbUpdateTime", int(math.floor(time.time())))
                 result = {}
 
                 # 如果有 price 字段则使用 price 字段作为当前价格，否则使用买一价格
@@ -70,32 +70,35 @@ def main():
                 redisKey = "R_" + vals["symbol"]
 
 
-                r.set(redisKey, str(result).replace("'", "\""))
-
+                # redisConn.set(redisKey, str(result).replace("'", "\""))
+                data2redis[redisKey] = str(result).replace("'", "\"")
                 if config.STAT_SETTINGS["enabled"] == "true":
                     vals["date"] = datetime.now()
                     vals["event"] = time.time()
-                    r.hmset(prefix + vals["symbol"], vals)
-                    r.incr(statPrefix + "ALL")
+                    redisConn.hmset(prefix + vals["symbol"], vals)
+                    redisConn.incr(statPrefix + "ALL")
 
                     eventTime = int(math.floor(vals["event"]))
                     cTime = vals["ctime"]
                     diffTime = eventTime - cTime
                     # print eventTime,cTime,diffTime
                     if diffTime <= 1:
-                        r.incr(statPrefix + "1s")
+                        redisConn.incr(statPrefix + "1s")
                     elif diffTime <= 3:
-                        r.incr(statPrefix + "3s")
+                        redisConn.incr(statPrefix + "3s")
                     elif diffTime <= 5:
-                        r.incr(statPrefix + "5s")
+                        redisConn.incr(statPrefix + "5s")
                     elif diffTime <= 10:
-                        r.incr(statPrefix + "10s")
+                        redisConn.incr(statPrefix + "10s")
                     elif diffTime > 10:
-                        r.incr(statPrefix + "B10")
+                        redisConn.incr(statPrefix + "B10")
 
                 print datetime.now(), time.time(), vals["ctime"], vals["symbol"], vals["bid"], vals["price"], vals[
                     "ask"], vals[
                     "high"], vals["low"]
+
+        if len(data2redis) > 0:
+            redisConn.mset(data2redis)
 
     stream.close()
 
